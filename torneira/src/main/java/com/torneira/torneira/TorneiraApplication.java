25e14a37b411fd4f5c7a7da001839c205380d0ab
isakawecki <isakawecki@gmail.com>
2025-01-27 14:22:46 -0300

    Add files via upload

---
+++ torneira/src/main/java/com/torneira/torneira/TorneiraApplication.java
@@ -0,0 +1,13 @@
+package com.torneira.torneira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneiraApplication.class, args);
+	}
+
+}
---
+++ torneira/src/test/java/com/torneira/torneira/TorneiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torneira.torneira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
