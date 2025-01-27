5e60afc269207bb6e2311370b540c5eb0cfa5d11
isakawecki <isakawecki@gmail.com>
2025-01-27 15:57:49 -0300

    Delete torneira/torneira/src/main/java/com/torneira/torneira directory

--- torneira/torneira/src/main/java/com/torneira/torneira/controller/ControllerTorneira.java
+++
@@ -1,5 +0,0 @@
-package com.torneira.torneira.controller;
-
-public class ControllerTorneira {
-
-}
--- torneira/torneira/src/main/java/com/torneira/torneira/dto/torneiraDto.java
+++
@@ -1,5 +0,0 @@
-package com.torneira.torneira.dto;
-
-public record torneiraDto(String marca, String modelo, String cor, String id) {
-
-}
